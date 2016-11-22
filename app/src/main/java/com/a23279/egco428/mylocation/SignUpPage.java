4649e8ec3e9d2368572af03bc69251eb633d5413
walaitorn <33399pam@gmail.com>
2016-11-22 16:23:36 +0700

    finish

--- app/src/main/java/com/a23279/egco428/mylocation/SignUpPage.java
+++ app/src/main/java/com/a23279/egco428/mylocation/SignUpPage.java
@@ -1,5 +1,9 @@
 package com.a23279.egco428.mylocation;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,19 +17,17 @@ import android.widget.EditText;
 import java.text.DecimalFormat;
 import java.util.Random;
 
-public class SignUpPage extends AppCompatActivity {
+public class SignUpPage extends AppCompatActivity implements SensorEventListener {
 
     private long lastUpdate;
+    private int countShake = 0;
     private boolean isPress=false;
-    //    private String Username;
-//    private String Password;
-//    private String Latitude;
-//    private String Longitude;
     private LoginDataSource dataSource;
     EditText username;
     EditText password;
     EditText longitude;
     EditText latitude;
+    private SensorManager sensorManager;
     Button addBtn;
     Button randomBtn;
 
@@ -38,6 +40,7 @@ public class SignUpPage extends AppCompatActivity {
         //mActionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#E3F2FD")));
         mActionBar.setTitle(Html.fromHtml("<font color=\"white\">" + "Sign-up Page" + "</font>"));
         mActionBar.setDisplayHomeAsUpEnabled(true);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
         username = (EditText)findViewById(R.id.EditName);
         password = (EditText)findViewById(R.id.EditPass);
@@ -48,6 +51,53 @@ public class SignUpPage extends AppCompatActivity {
         dataSource.open();
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            getAccelerometer(event);
+        }
+    }
+
+    private void getAccelerometer(SensorEvent event) {
+        float[] values = event.values;
+        // Movement
+        float x = values[0];
+        float y = values[1];
+        float z = values[2];
+
+        float accelationSquareRoot = (x * x + y * y + z * z)
+                / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
+
+        long actualTime = System.currentTimeMillis();
+        if (accelationSquareRoot >= 4) {
+            if (actualTime - lastUpdate < 700) {
+                return;
+            }
+            countShake += 1;
+            lastUpdate = actualTime;
+            if (countShake > 1) {
+                pressRandom();
+                countShake = 0;
+                return;
+            }
+        }
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), sensorManager.SENSOR_DELAY_NORMAL);// get sensor when open app again
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this); // close sensor when exit app
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -84,7 +134,11 @@ public class SignUpPage extends AppCompatActivity {
 
     }
 
-    public void pressRandom(View view){
+    public void random(View view){
+        pressRandom();
+    }
+
+    public void pressRandom(){
 
         Random r = new Random();
         double randomLati = -85.000000 + (85.000000 - (-85.000000)) * r.nextDouble();
